4126d7e7a3031a70a86de6ec5d0202c1da8fb8fa
sugam <sugamkarki7058@gmail.com>
2021-02-11 21:19:29 +0545

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./App.css";
+import Banana, { Sugam } from "./components/Aaditya";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <Banana />
+            <Sugam />
+        </div>
+    );
+}
+export default App;
---
+++ src/components/Aaditya.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+// class Aaditya extends Component {
+//     constructor() {
+//         super();
+//     }
+//     render() {
+//         return <h1>My name is aaditya</h1>;
+//     }
+// }
+function Aaditya() {
+    return <h1>My Name is aaditya</h1>;
+}
+
+export function Sugam() {
+    return <h1>My name is sugam</h1>;
+}
+
+export default Aaditya;
